4126f6cde965f0bea68a9f2412b9000142be54e4
Kumar <Narender.Kumar@elektrobit.com>
2022-07-29 10:29:43 +0530

    Log added in HelloWorld/

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 /**
  * main activity
 * */
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MyActivity";
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +21,17 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.Hello);
 //        Set the text of textView to the required text
         textView.setText("Hello World!");
+        /*
+        ***  logs   ***
+        * */
+        Log.d(TAG, "This is a log for DEBUG");
+
+        Log.e(TAG, "This is a log for ERROR");
+
+        Log.v(TAG, "This is a log for VERBOSE");
+
+        Log.i(TAG, "This is a log for INFO");
+
+        Log.w(TAG, "this is a log for WARN");
     }
 }
