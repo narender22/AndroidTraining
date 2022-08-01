efa28a0b997b973dd0354a7ebeb3279478e34c2c
Kumar <Narender.Kumar@elektrobit.com>
2022-08-01 14:33:51 +0530

    Explicit Intent fixed

--- ExplicitIntentExample/app/src/main/java/com/example/explicitintentexample/MainActivity.java
+++ ExplicitIntentExample/app/src/main/java/com/example/explicitintentexample/MainActivity.java
@@ -23,10 +23,17 @@ public class MainActivity extends AppCompatActivity {
      public void onIndexSendClick(View view) {
 //        first set the intent to the second activity
          Intent intent = new Intent(this, SecondActivity.class);
-//
+
+//         editText have thee index of the image to be displayed
          EditText editText = findViewById(R.id.editText);
+
+//         convert the index entered to string for usage
          String index = editText.getText().toString();
+
+//         load the index in intent
          intent.putExtra("imageIndex", index);
+
+//         pass the index to second activity to show the image accordingly
          startActivity(intent);
      }
 }
