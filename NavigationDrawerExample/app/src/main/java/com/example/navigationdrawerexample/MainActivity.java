f6631a405a1039129124c6d08e59249c81d8baf6
narender22 <narendarkumar22@gmail.com>
2022-08-17 17:53:40 +0530

    comments added to navigation drawer

--- NavigationDrawerExample/app/src/main/java/com/example/navigationdrawerexample/MainActivity.java
+++ NavigationDrawerExample/app/src/main/java/com/example/navigationdrawerexample/MainActivity.java
@@ -11,19 +11,23 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
-
+/*
+* Main activity
+* */
 public class MainActivity extends AppCompatActivity {
-
+//    create an instance of views to be used
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
-
+/*
+* When app is created
+* */
     @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//        set view id for instances
         drawerLayout = findViewById(R.id.drawerLayout);
         navigationView = findViewById(R.id.navView);
         toolbar = findViewById(R.id.toolBar);
@@ -31,19 +35,28 @@ public class MainActivity extends AppCompatActivity {
 //        set tool bar
         setSupportActionBar(toolbar);
 
+//        set action for app drawer
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+//        set listener for drawer
         drawerLayout.addDrawerListener(toggle);
 
+//        sync drawer according to action
         toggle.syncState();
 
+//        set the task to be performed on click of some button/view in app drawer
         navigationView.setNavigationItemSelectedListener(item -> {
             switch (item.getItemId()){
+//                when home button is clicked
                 case R.id.home:
+//                    show a toast
                     Toast.makeText(MainActivity.this, "Home button clicked", Toast.LENGTH_SHORT).show();
+//                    close the drawer when clicked
                     drawerLayout.closeDrawer(GravityCompat.START);
+//                when call button is clicked
                 case R.id.call:
                     Toast.makeText(MainActivity.this, "Home button clicked", Toast.LENGTH_SHORT).show();
                     drawerLayout.closeDrawer(GravityCompat.START);
+//                on setting button click
                 case R.id.setting:
                     Toast.makeText(MainActivity.this, "Home button clicked", Toast.LENGTH_SHORT).show();
                     drawerLayout.closeDrawer(GravityCompat.START);
@@ -54,12 +67,17 @@ public class MainActivity extends AppCompatActivity {
             return true;
         });
     }
-
+/*
+* If user presses the back button
+* */
     @Override
     public void onBackPressed() {
+//        check if drawer is open or not
         if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+//            if drawer is open close it
             drawerLayout.closeDrawer(GravityCompat.START);
         }
+//        is drawer is not open do default set task
         else{
             super.onBackPressed();
         }
