71c171f1c4a92f69d194575021c082c00bffb2bc
narender22 <narendarkumar22@gmail.com>
2022-08-24 14:37:24 +0530

    done content provider example

---
+++ ContentProviderExample/app/src/main/java/com/example/contentproviderexample/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.contentproviderexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "CONTACT_PROVIDER_DEMO";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void btnGetContact(View view) {
+        getPhoneContacts();
+    }
+
+    private void getPhoneContacts(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_CONTACTS}, 0);
+        }
+
+        ContentResolver contentResolver = getContentResolver();
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        Cursor cursor = contentResolver.query(uri, null, null, null, null);
+        Log.d(TAG, "Total # of contacts ::: "+ cursor.getCount());
+        if (cursor.getCount() > 0){
+            while (cursor.moveToNext()){
+                @SuppressLint("Range") String contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                @SuppressLint("Range") String contactNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                Log.d(TAG, "Contact Name     :::   "+ contactName + "    Ph     :::    "+ contactNumber);
+            }
+        }
+        cursor.close();
+    }
+}
